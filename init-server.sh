b532a6a36074d3daebb7cca3371a6ae439652496
Dennis Kasper <dennis.m.kasper@gmail.com>
2024-12-13 18:46:58 +0100

    docs: add non-root user to server setup

--- init-server.sh
+++ init-server.sh
@@ -1,7 +1,38 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # Hint: Manually update and upgrade the systemn by running the following command: 
 # sudo apt update && sudo apt upgrade -y && sudo reboot
+
+# Prompt for the new username
+read -p 'Enter the username for the new non-root user: ' new_user
+
+# Add a non-root passwordless user
+sudo adduser --disabled-password $new_user
+sudo usermod -aG sudo $new_user
+
+# Add the NOPASSWD directive for the new user to /etc/sudoers.d
+echo "$new_user ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$new_user > /dev/null
+
+# Switch to the new user
+su - $new_user
+
+# Set up SSH key-based login for the new user
+sudo mkdir -p /home/$new_user/.ssh
+
+# Hint: To retrieve the SSH public key from your local machine, run:
+# cat .ssh/id_ed25519.pub
+# Copy the output of this command and paste it when prompted below.
+read -p "Enter the public SSH key for the new user: " ssh_key
+
+echo "$ssh_key" | sudo tee /home/$new_user/.ssh/authorized_keys > /dev/null
+sudo chmod 700 /home/$new_user/.ssh
+sudo chmod 600 /home/$new_user/.ssh/authorized_keys
+sudo chown -R $new_user:$new_user /home/$new_user/.ssh
+
+# Secure SSH configuration
+# sudo sed -i '/^PermitRootLogin/c\PermitRootLogin no' /etc/ssh/sshd_config
+sudo sed -i '/^PasswordAuthentication/c\PasswordAuthentication no' /etc/ssh/sshd_config
+sudo systemctl restart ssh
 
 # Install necessary packages
 sudo apt install -y curl nginx snapd ufw nano
